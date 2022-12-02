5e1dfb1aeadcc8052b5606416a0093381f665972
MohamedAmineSafi04 <mohammedameenalsafi@gmail.com>
2022-12-02 09:23:25 +0100

    Home Page ListView AND Template Setup

--- feed/views.py
+++ feed/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+# from django.shortcuts import render NOT USING FUNCTION BASED VIEW
+from django.views.generic import ListView
+from .models import Post
 
 # Create your views here.
+class HomePage(ListView):
+    http_method_names = ['get']
+    template_name = 'homepage.html'
+    model = Post
+    context_object_name = 'posts'
+    queryset = Post.objects.all().order_by('-id')[0:30] #Get only 30 posts
