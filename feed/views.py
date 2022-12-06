a35f6f5393e13c58a2c9e4a3bf2913372e2ed9b1
MohamedAmineSafi04 <mohammedameenalsafi@gmail.com>
2022-12-06 10:46:19 +0100

    Post DetailView

--- feed/views.py
+++ feed/views.py
@@ -1,5 +1,5 @@
 # from django.shortcuts import render NOT USING FUNCTION BASED VIEW
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 from .models import Post
 
 # Create your views here.
@@ -9,3 +9,9 @@ class HomePage(ListView):
     model = Post
     context_object_name = 'posts'
     queryset = Post.objects.all().order_by('-id')[0:30] #Get only 30 posts
+
+class PostDetailView(DetailView):
+    http_method_names = ['get']
+    template_name = "feed/detail.html"
+    model = Post
+    context_object_name = 'post' #default is 'object' so instead of {{object.text}} use {{post.text}}
