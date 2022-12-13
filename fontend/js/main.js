337b8087d662754921474d34e82564b1553009cc
MohamedAmineSafi04 <mohammedameenalsafi@gmail.com>
2022-12-13 17:15:28 +0100

    Following and unfollowing users

--- fontend/js/main.js
+++ fontend/js/main.js
@@ -62,3 +62,27 @@ $(document).on("click", '.js-toggle-modal', function(e){
         }
     });
 })
+.on('click', '.js-follow', function(e){
+    e.preventDefault()
+    const action = $(this).attr("data-action")
+    $.ajax({
+        type: 'POST',
+        url: $(this).data("url"),
+        data: {
+            action: action, // not cached
+            username: $(this).data("username"), // cached
+        },
+        success: (data) => {
+            console.log(data)
+            $(".js-follow-text").text(data.wording)
+            if (action == "follow") {
+                $(this).attr('data-action', 'unfollow')
+            } else {
+                $(this).attr('data-action', 'follow')
+            }
+        },
+        error: (error) => {
+            console.warn(error)
+        }
+    });
+})
--- profiles/views.py
+++ profiles/views.py
@@ -1,8 +1,11 @@
 # from django.shortcuts import render
-from django.views.generic import DetailView
+from django.views.generic import DetailView, View
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth.models import User
+from django.http import JsonResponse, HttpResponseBadRequest
 
 from feed.models import Post
+from followers.models import Follower
 
 # Create your views here.
 class ProfileDetailView(DetailView):
@@ -18,3 +21,38 @@ class ProfileDetailView(DetailView):
         context = super().get_context_data(**kwargs)
         context['total_posts'] = Post.objects.filter(author=user).count()
         return context
+
+class FollowView(LoginRequiredMixin, View):
+    http_method_names = ['post']
+    def post(self, request, *args, **kwargs):
+        data = request.POST.dict()
+
+        if "action" not in data or "username" not in data:
+            return HttpResponseBadRequest("Missing Data")
+        
+        try:
+            other_user = User.objects.get(username=data['username'])
+        except User.DoesNotExist:
+            return HttpResponseBadRequest("Missing User")
+
+        if data['action'] == 'follow':
+            follower, created = Follower.objects.get_or_create(
+                followed_by=request.user,
+                following=other_user
+            )
+        else:
+            try:
+                follower = Follower.objects.get(
+                    followed_by=request.user,
+                    following=other_user
+                )
+            except Follower.DoesNotExist:
+                follower = None
+            
+            if follower:
+                follower.delete()
+
+        return JsonResponse({
+            'success': True,
+            'wording': "Unfollow" if data['action'] == 'follow' else "Follow"
+        })
